3ea36f4b8ab34c6f33fcc9b04f4c901e109cdde7
Dean <dean.g.leong@gmail.com>
2020-12-09 13:34:08 -0500

    template of project screens almost done needs more styling carosuel for screens? check greenhouse

--- client/src/screens/Project1.jsx
+++ client/src/screens/Project1.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import './Projects.css'
+import { Link } from 'react-router-dom'
 
 function Project1(props) {
   return (
-    <div>
+    <div className="project-page-container">
       <h2>Leauge Champions Finder</h2>
-        <img
-          src="https://raw.githubusercontent.com/DeanLeong/leong_portfolio/develop/client/src/assets/league_champions_screen.png"
-          className="project-page-screen"
-          alt="league champions screen">
-        </img>
-        <p className="project-description">
-          The League Champions Finder is built in regular JavaScript, HTML and CSS. 
-          Using calls to an API containing names and basic stats of the Champions in the League of Legends
-          users can search for champions by their class. Once they have been searched, the champion's splash image,
-          name and tile are displayed. Each time the search button is hit six random champions of the appropriate class are displayed.
-       </p>
+      <div className="title-image-description">
+        <a href="https://deanleong.github.io/league_champions/" target="_blank" rel="noreferrer">
+          <img
+            src="https://raw.githubusercontent.com/DeanLeong/leong_portfolio/develop/client/src/assets/league_champions_screen.png"
+           className="project-page-screen"
+           alt="league champions screen">
+         </img>
+        </a>
+         <p className="project-description">
+            The League Champions Finder is built in regular JavaScript, HTML and CSS. 
+            Using calls to an API containing names and basic stats of the Champions in the League of Legends
+            users can search for champions by their class. Once they have been searched, the champion's splash image,
+            name and tile are displayed. Each time the search button is hit, six random champions of the appropriate class are displayed.
+        </p>
+       </div>
+      <div className="links-container">
+        <a href="https://github.com/DeanLeong/league_champions" target="_blank" rel="noreferrer" className="project-links"> GitHub Repo </a>
+        <a href="https://deanleong.github.io/league_champions/" target="_blank" rel="noreferrer" className="project-links"> Live Site </a>
+      </div>
+      <p>Other Projects:</p>
+      <div className="other-projects-container">
+      <Link to="/greenhouse">
+          <img
+            src="https://i.imgur.com/Gqvwv0U.png"
+            className="other-project-screen"
+            alt="greenhouse screen">
+          </img>
+        </Link>
+        <Link to="/liftlog">
+          <img
+            src="https://i.imgur.com/wHRy00R.png"
+            className="other-project-screen"
+            alt="liftlog screen">
+          </img>
+        </Link>
+        <Link to="/the-garage">
+          <img
+            src="https://raw.githubusercontent.com/DeanLeong/leong_portfolio/develop/client/src/assets/the_garage_screen.png"
+            className="other-project-screen"
+            alt="league champions screen">
+          </img>
+        </Link>
+      </div>
     </div>
   );
 }
