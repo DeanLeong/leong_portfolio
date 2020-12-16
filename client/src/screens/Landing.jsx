7f25af45e0952289f0c31301daafe261c83acf4c
Dean <dean.g.leong@gmail.com>
2020-12-16 12:08:04 -0500

    added a typing effect from typical

--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import "./Landing.css"
+import { Link } from 'react-router-dom'
+import Typical from 'react-typical'
 
 function Landing(props) {
+
+  
+
   return (
-    <div>
-      <h1>Hello!!!</h1>
+    <div className="landing-container">
+      <div className="landing-info-container">
+        <p className="landing-welcome-msg">Hello! My name is Dean Leong, welcome to my portfolio!</p>
+          <Typical
+            steps={['Hello', 1000, 'Hello world!', 500]}
+            loop={Infinity}
+            wrapper="p"
+            className="landing-welcome-msg"
+          />
+        <Link to="/home"><button>Enter</button></Link>
+      </div>
     </div>
   );
 }
